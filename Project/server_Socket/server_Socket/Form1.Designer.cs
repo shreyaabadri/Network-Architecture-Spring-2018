f181ed4e45e14e1455aeb8b7aebd6e949a202e89
shreyaabadri <31324449+shreyaabadri@users.noreply.github.com>
2018-05-04 17:04:48 -0500

    server

---
+++ Project/server_Socket/server_Socket/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace server_Socket
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.btnServer = new System.Windows.Forms.Button();
+			this.lblServerStatus = new System.Windows.Forms.Label();
+			this.lblServerCurrentStatus = new System.Windows.Forms.Label();
+			this.lblSelectActionn = new System.Windows.Forms.Label();
+			this.cbSelectOperation = new System.Windows.Forms.ComboBox();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.lblData = new System.Windows.Forms.Label();
+			this.lblCurrentstatus = new System.Windows.Forms.Label();
+			this.lblActualData = new System.Windows.Forms.Label();
+			this.lblFileName = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// btnServer
+			// 
+			this.btnServer.BackColor = System.Drawing.Color.SteelBlue;
+			this.btnServer.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+			this.btnServer.Location = new System.Drawing.Point(177, 7);
+			this.btnServer.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.btnServer.Name = "btnServer";
+			this.btnServer.Size = new System.Drawing.Size(989, 72);
+			this.btnServer.TabIndex = 0;
+			this.btnServer.Text = "START SERVER";
+			this.btnServer.UseVisualStyleBackColor = false;
+			this.btnServer.Click += new System.EventHandler(this.btnServer_Click);
+			// 
+			// lblServerStatus
+			// 
+			this.lblServerStatus.AutoSize = true;
+			this.lblServerStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblServerStatus.ForeColor = System.Drawing.Color.Black;
+			this.lblServerStatus.Location = new System.Drawing.Point(90, 99);
+			this.lblServerStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.lblServerStatus.Name = "lblServerStatus";
+			this.lblServerStatus.Size = new System.Drawing.Size(395, 39);
+			this.lblServerStatus.TabIndex = 2;
+			this.lblServerStatus.Text = "Server Current Status :";
+			// 
+			// lblServerCurrentStatus
+			// 
+			this.lblServerCurrentStatus.AutoSize = true;
+			this.lblServerCurrentStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblServerCurrentStatus.ForeColor = System.Drawing.Color.Green;
+			this.lblServerCurrentStatus.Location = new System.Drawing.Point(363, 99);
+			this.lblServerCurrentStatus.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.lblServerCurrentStatus.Name = "lblServerCurrentStatus";
+			this.lblServerCurrentStatus.Size = new System.Drawing.Size(0, 39);
+			this.lblServerCurrentStatus.TabIndex = 3;
+			// 
+			// lblSelectActionn
+			// 
+			this.lblSelectActionn.AutoSize = true;
+			this.lblSelectActionn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblSelectActionn.ForeColor = System.Drawing.Color.Black;
+			this.lblSelectActionn.Location = new System.Drawing.Point(88, 177);
+			this.lblSelectActionn.Name = "lblSelectActionn";
+			this.lblSelectActionn.Size = new System.Drawing.Size(381, 39);
+			this.lblSelectActionn.TabIndex = 4;
+			this.lblSelectActionn.Text = "Select Mode               :";
+			// 
+			// cbSelectOperation
+			// 
+			this.cbSelectOperation.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.cbSelectOperation.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.cbSelectOperation.FormattingEnabled = true;
+			this.cbSelectOperation.Items.AddRange(new object[] {
+            "<---Select--->",
+            "Send",
+            "Receive"});
+			this.cbSelectOperation.Location = new System.Drawing.Point(514, 174);
+			this.cbSelectOperation.Name = "cbSelectOperation";
+			this.cbSelectOperation.Size = new System.Drawing.Size(287, 47);
+			this.cbSelectOperation.TabIndex = 5;
+			this.cbSelectOperation.SelectedIndexChanged += new System.EventHandler(this.cbSelectOperation_SelectedIndexChanged);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			// 
+			// lblData
+			// 
+			this.lblData.AutoSize = true;
+			this.lblData.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblData.ForeColor = System.Drawing.Color.Black;
+			this.lblData.Location = new System.Drawing.Point(88, 320);
+			this.lblData.Name = "lblData";
+			this.lblData.Size = new System.Drawing.Size(393, 39);
+			this.lblData.TabIndex = 6;
+			this.lblData.Text = "File Status                   :";
+			this.lblData.Click += new System.EventHandler(this.lblData_Click);
+			// 
+			// lblCurrentstatus
+			// 
+			this.lblCurrentstatus.AutoSize = true;
+			this.lblCurrentstatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblCurrentstatus.ForeColor = System.Drawing.Color.Green;
+			this.lblCurrentstatus.Location = new System.Drawing.Point(626, 250);
+			this.lblCurrentstatus.Name = "lblCurrentstatus";
+			this.lblCurrentstatus.Size = new System.Drawing.Size(0, 39);
+			this.lblCurrentstatus.TabIndex = 7;
+			this.lblCurrentstatus.Click += new System.EventHandler(this.lblCurrentstatus_Click);
+			// 
+			// lblActualData
+			// 
+			this.lblActualData.AutoSize = true;
+			this.lblActualData.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblActualData.Location = new System.Drawing.Point(528, 320);
+			this.lblActualData.Name = "lblActualData";
+			this.lblActualData.Size = new System.Drawing.Size(0, 39);
+			this.lblActualData.TabIndex = 8;
+			this.lblActualData.Click += new System.EventHandler(this.lblActualData_Click);
+			// 
+			// lblFileName
+			// 
+			this.lblFileName.AutoSize = true;
+			this.lblFileName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblFileName.Location = new System.Drawing.Point(922, 177);
+			this.lblFileName.Name = "lblFileName";
+			this.lblFileName.Size = new System.Drawing.Size(0, 39);
+			this.lblFileName.TabIndex = 9;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(24F, 46F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+			this.ClientSize = new System.Drawing.Size(1297, 519);
+			this.Controls.Add(this.lblFileName);
+			this.Controls.Add(this.lblActualData);
+			this.Controls.Add(this.lblCurrentstatus);
+			this.Controls.Add(this.lblData);
+			this.Controls.Add(this.cbSelectOperation);
+			this.Controls.Add(this.lblSelectActionn);
+			this.Controls.Add(this.lblServerCurrentStatus);
+			this.Controls.Add(this.lblServerStatus);
+			this.Controls.Add(this.btnServer);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.ForeColor = System.Drawing.Color.Blue;
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.MaximizeBox = false;
+			this.MinimizeBox = false;
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "SERVER";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnServer;
+        private System.Windows.Forms.Label lblServerStatus;
+        private System.Windows.Forms.Label lblServerCurrentStatus;
+        private System.Windows.Forms.Label lblSelectActionn;
+        private System.Windows.Forms.ComboBox cbSelectOperation;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label lblData;
+        private System.Windows.Forms.Label lblCurrentstatus;
+        private System.Windows.Forms.Label lblActualData;
+        private System.Windows.Forms.Label lblFileName;
+    }
+}
+
---
+++ Project/server_Socket/server_Socket/Form1.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+
+namespace server_Socket
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        TcpListener serverSocketListener;
+        TcpClient clientSockets;
+        bool server_check=false;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cbSelectOperation.Enabled = false;
+        }
+
+        private void btnServer_Click(object sender, EventArgs e)
+        {
+            //server_check = false;
+            if (btnServer.Text == "START SERVER" && server_check == false)
+            {
+                // Starting the server
+                serverSocketListener = new TcpListener(8888);
+                clientSockets = default(TcpClient);
+                lblServerCurrentStatus.Text = "Server started successfully, waiting for the clients to connect...";
+                serverSocketListener.Start();
+                
+                clientSockets = serverSocketListener.AcceptTcpClient();
+                lblServerCurrentStatus.Text = "Client Connected successfully.";
+                cbSelectOperation.Enabled = true;
+                cbSelectOperation.SelectedIndex = 0;
+                lblCurrentstatus.Text = "Waiting for data transfer";
+            }
+            else if (btnServer.Text == "STOP SERVER" && server_check == true)
+            {
+                clientSockets.Close();
+                serverSocketListener.Stop();
+                lblServerCurrentStatus.Text = "Server stopped successfully.";
+            }
+            if (server_check == false)
+            {
+                server_check = true;
+                btnServer.Text = "STOP SERVER";
+               
+            }
+            else if (server_check == true)
+            {
+                server_check = false;
+                btnServer.Text = "START SERVER";
+            }
+        }
+
+        private void cbSelectOperation_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            NetworkStream networkStream;
+            int retry = 0;
+            if (cbSelectOperation.SelectedIndex == 1  && cbSelectOperation.Text == "Send")
+            {
+                lblCurrentstatus.Text = "Sending data..";
+                cbSelectOperation.Enabled = false;
+                string[] fileContent = null;
+                networkStream = clientSockets.GetStream();
+                try
+                {
+                    openFileDialog1.ShowDialog();
+                    lblFileName.Text = "File: " + System.IO.Path.GetFileName(openFileDialog1.FileName);
+                    fileContent = System.IO.File.ReadAllLines(openFileDialog1.FileName.ToString());
+
+                    for (int i = 0; i < fileContent.Length; i++)
+                    {
+                        if (fileContent[i] == "")
+                        {
+                            string line = "emptyline";
+                            lblActualData.Text = "empty line data..";
+                            Byte[] sendBytes = frameForm(line, fileContent.Length, i);
+                            networkStream.Write(sendBytes, 0, sendBytes.Length);
+                        }
+                        else
+                        {
+                            lblActualData.Text = fileContent[i].ToString();
+                            Byte[] sendBytes = frameForm(fileContent[i], fileContent.Length, i);
+                            networkStream.Write(sendBytes, 0, sendBytes.Length);
+                        }
+                        System.Threading.Thread.Sleep(10);
+                        byte[] bytesFrom = new byte[10];
+                        networkStream.Read(bytesFrom, 0, 10);
+                        bool check = ackCheck(bytesFrom);
+                        if (check == false)
+                        {
+                            retry = retry + 1;
+                            i = i - 1;
+                        }
+                        if (retry == 3)
+                        {
+                            break;
+                        }
+                    }
+                    if (retry == 3)
+                    {
+                        // closing packet sending
+                        lblActualData.Text = "Sending stopped due to error..";
+                        byte[] closeFrame = frameForm("stop", 1, 1);
+                        networkStream.Write(closeFrame, 0, closeFrame.Length);
+                        lblActualData.Text = "File transfer stopped..";
+                    }
+                    else
+                    {
+                        // closing packet sending
+                        lblActualData.Text = "Sending closing packet..";
+                        byte[] closeFrame = frameForm("close", 1, 1);
+                        networkStream.Write(closeFrame, 0, closeFrame.Length);
+                        lblActualData.Text = "File Sent successfully..";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("File selection failure/file not found, please check..");
+                    cbSelectOperation.SelectedIndex = 0;
+                }
+                networkStream.Flush();
+                cbSelectOperation.SelectedIndex = 0;
+            }
+            else if (cbSelectOperation.SelectedIndex == 2 && cbSelectOperation.Text == "Receive")
+            {
+                lblCurrentstatus.Text = "Receiving data..";
+                string data_stop = "";
+                cbSelectOperation.Enabled = false;
+                bool loop_check = true;
+                System.IO.StreamWriter file = new System.IO.StreamWriter(@"C:\Users\Shreya\Documents\Writehere.txt");
+                while (loop_check == true)
+                {
+                    try
+                    {
+                        networkStream = clientSockets.GetStream();
+                        byte[] bytesFrom = new byte[65536];
+                        networkStream.Read(bytesFrom, 0, (int)clientSockets.ReceiveBufferSize);
+                        Array.Resize(ref bytesFrom, 6 + bytesFrom[3]);
+                        int totalPkts = bytesFrom[1];
+                        int currentPkt = bytesFrom[2];
+                        //if (totalPkts == currentPkt)
+                        //{ break; }
+                        string data = parseData(bytesFrom);
+                        if (data == "close")
+                        {
+                            break;
+                        }
+                        if (data == "stop")
+                        {
+                            data_stop = "stop";
+                            break;
+                        }
+                        if (data == "emptyline")
+                        {
+                            file.WriteLine("\n");
+                            lblActualData.Text = "Empty line data..";
+                            networkStream.Write(ackframeForm("Pass"), 0, ackframeForm("Pass").Length);
+                        }
+                        else if (data != "")
+                        {
+                            lblActualData.Text = data;
+                            //have to write into a file + need to send the ack packet
+                            networkStream.Write(ackframeForm("Pass"), 0, ackframeForm("Pass").Length);
+                            file.WriteLine(data);
+                        }
+                        else
+                        {
+                            networkStream.Write(ackframeForm("Fail"), 0, ackframeForm("Fail").Length);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.ToString());
+                        file.Close();
+                        cbSelectOperation.SelectedIndex = 0;
+                    }
+                    if (data_stop == "stop")
+                    {
+                        cbSelectOperation.SelectedIndex = 0;
+                        lblActualData.Text = "File transfer failed due to checksum error..";
+                       
+                    }
+                    else
+                    {
+                        lblActualData.Text = "File received successfully..";
+                        cbSelectOperation.SelectedIndex = 0;
+                    }
+                }
+                file.Close();
+            }
+            lblCurrentstatus.Text = "File transfer successful..";
+            cbSelectOperation.Enabled = true;
+        }
+        public byte[] frameForm(string lineData,int totalLength, int currentPktNum)
+        {
+            byte[] data_frm = new byte[65536];
+            try
+            {
+                int index = 0;
+                data_frm[index++] = 0xaa; //Start of the packet - unique identification
+                data_frm[index++] = (byte)totalLength; //number of packets
+                data_frm[index++] = (byte)currentPktNum; //current packet number
+                data_frm[index++] = (byte)lineData.Length; //current data length
+                Byte[] dataBytes = Encoding.ASCII.GetBytes(lineData); //data
+                Buffer.BlockCopy(dataBytes, 0, data_frm, index, dataBytes.Length); //copying data to original array
+                data_frm[dataBytes.Length-1 + index+1] = calCheckSum(data_frm,1,dataBytes.Length+index); //checksum portion
+                data_frm[dataBytes.Length-1 + index+2] = 0x99; //end of packet - unique identification
+                Array.Resize(ref data_frm, dataBytes.Length + index+2); //resizing the array to actual size
+            }
+            catch (Exception ex)
+            {
+                data_frm = null;
+            }
+            return data_frm;
+        }
+        public byte[] ackframeForm(string data)
+        {
+            byte[] ack_frm = new byte[65536];
+            try
+            {
+                int index = 0;
+                ack_frm[index++] = 0xaa; //Start of the packet - unique identification
+                ack_frm[index++] = 0x01; //number of packets
+                ack_frm[index++] = 0x01; //current packet number
+                ack_frm[index++] = 0x04; //current data length
+                Byte[] dataBytes = Encoding.ASCII.GetBytes(data); //data
+                Buffer.BlockCopy(dataBytes, 0, ack_frm, index, dataBytes.Length); //copying data to original array
+                ack_frm[dataBytes.Length - 1 + index + 1] = calCheckSum(ack_frm, 1, dataBytes.Length + index); //checksum portion
+                ack_frm[dataBytes.Length - 1 + index + 2] = 0x99; //end of packet - unique identification
+                Array.Resize(ref ack_frm, dataBytes.Length + index + 2); //resizing the array to actual size
+            }
+            catch (Exception ex)
+            {
+                ack_frm = null;
+            }
+            return ack_frm;
+        }
+        public string parseData(byte[] receiveData)
+        {
+            string actualData = "";
+            try
+            {
+                int totalPckts = receiveData[1]; //retriving totoal number of packets count.
+                int currentPktNum = receiveData[2]; //current packet number
+                int dataLength = receiveData[3]; //actual data length
+                byte chkSum = calCheckSum(receiveData, 1, receiveData.Length - 2); //Re-calculate the checksum
+                if (chkSum == receiveData[receiveData.Length - 2])
+                {
+                    byte[] data = new byte[dataLength];
+                    Array.Copy(receiveData, 4, data, 0, dataLength);
+                    actualData = System.Text.Encoding.ASCII.GetString(data);
+                }
+                else
+                {
+                    actualData = "";
+                    MessageBox.Show("Checksum Error Found.");
+                }
+            }
+            catch (Exception ex)
+            {
+                actualData = "";
+            }
+            return actualData;
+        }
+        public byte calCheckSum(byte[] data_frm,int startindex,int endindex)
+        {
+            byte chkSum = 0x0;
+            try
+            {
+                for (int i = startindex; i < endindex; i++)
+                {
+                    chkSum ^= data_frm[i];
+                }
+            }
+            catch (Exception ex)
+            {
+                chkSum = 0x0;
+            }
+            return chkSum;
+        }
+        public bool ackCheck(byte[] ack_pkt)
+        {
+            bool flag = false;
+            try
+            {
+                string ack = System.Text.Encoding.ASCII.GetString(ack_pkt);
+                if (ack.Contains("Pass") || ack.Contains("PASS") || ack.Contains("pass"))
+                {
+                    flag = true;
+                }
+                else { flag = false; }
+            }
+            catch (Exception ex)
+            {
+                flag = false;
+            }
+            return flag;
+        }
+
+		private void lblActualData_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void lblCurrentstatus_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void lblData_Click(object sender, EventArgs e)
+		{
+
+		}
+	}
+}
